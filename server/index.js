97cba84a78ce572af97c04bc116d6e3210b5fb7d
Mario de Biase <debiasej@gmail.com>
2018-01-31 21:37:18 +0100

    Add a middleware to avoid CORS

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const authRoutes = require('./routes/phones')
+const allowCrossDomain = require('./middlewares/cors')
 
 const app = express()
+app.use(allowCrossDomain)
 authRoutes(app)
 
 const PORT = process.env.PORT || 5000
