92f5659cc613e880f4b7e84d9bd1c8aafbf576e5
Mario de Biase <debiasej@gmail.com>
2018-02-01 00:34:56 +0100

    Install Sleep to simulate network latency

--- server/routes/phones.js
+++ server/routes/phones.js
@@ -1,7 +1,10 @@
+var sleep = require('sleep')
+
 const data = require('../mocks/phones.json')
 
 const routes = (app) => {
   app.get('/phones', (req, res) => {
+    sleep.msleep(3000) // Simulate network latency
     res.send(data)
   })
 }
